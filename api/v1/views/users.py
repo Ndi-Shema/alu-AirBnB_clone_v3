5fe64046ad927c42ed48edfba0a1bc26841d0a57
Ndi-Shema <f.shema@alustudent.com>
2023-04-27 07:47:04 +0200

    DBstorage and filestorage

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Module"""
+"""
+This module defines routes for managing User objects using Flask.
+"""
+
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response, abort, request
 from models import storage
-from flask import make_response, abort, request
 from models.user import User
 
 
 @app_views.route("/users", methods=['GET'], strict_slashes=False)
-def users():
-    """Retrieves the list of all User objects"""
+def get_users():
+    """
+    Retrieves the list of all User objects.
+
+    Returns:
+        A JSON response containing the list of User objects.
+    """
     users = storage.all(User).values()
     list_users = []
     for user in users:
@@ -18,8 +25,19 @@ def users():
 
 
 @app_views.route("/users/<user_id>", methods=['GET'], strict_slashes=False)
-def users_id(user_id):
-    """Retrieves a User object"""
+def get_user(user_id):
+    """
+    Retrieves a User object.
+
+    Args:
+        user_id (str): The ID of the User object to retrieve.
+
+    Returns:
+        A JSON response containing the User object.
+
+    Raises:
+        404: If no User object with the given ID is found.
+    """
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
@@ -28,7 +46,18 @@ def users_id(user_id):
 
 @app_views.route("/users/<user_id>", methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
-    """Deletes a User object"""
+    """
+    Deletes a User object.
+
+    Args:
+        user_id (str): The ID of the User object to delete.
+
+    Returns:
+        An empty JSON response.
+
+    Raises:
+        404: If no User object with the given ID is found.
+    """
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
@@ -39,7 +68,16 @@ def delete_user(user_id):
 
 @app_views.route("/users", methods=['POST'], strict_slashes=False)
 def create_user():
-    """Creates a User"""
+    """
+    Creates a User object.
+
+    Returns:
+        A JSON response containing the created User object.
+
+    Raises:
+        400: If the request does not contain a valid JSON body, or if the JSON body is missing
+             the required 'email' or 'password' fields.
+    """
     if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
     if "email" not in request.get_json():
@@ -55,7 +93,19 @@ def create_user():
 
 @app_views.route("/users/<user_id>", methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
-    """Updates a User object"""
+    """
+    Updates a User object.
+
+    Args:
+        user_id (str): The ID of the User object to update.
+
+    Returns:
+        A JSON response containing the updated User object.
+
+    Raises:
+        404: If no User object with the given ID is found.
+        400: If the request does not contain a valid JSON body.
+    """
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
