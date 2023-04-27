b7e4d589d02937c86035b2459b14ce2594ce7e26
Ndi-Shema <f.shema@alustudent.com>
2023-04-27 07:37:03 +0200

    DBstorage and filestorage

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Module"""
+"""
+This module defines views for Amenity objects.
+"""
+
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response, abort, request
 from models import storage
-from flask import make_response, abort, request
 from models.amenity import Amenity
 
 
 @app_views.route("/amenities", methods=['GET'], strict_slashes=False)
 def amenities():
-    """Retrieves the list of all Amenity objects"""
+    """
+    Retrieves the list of all Amenity objects.
+
+    Returns:
+        A JSON response with a list of all Amenity objects in the database.
+    """
     amenities = storage.all(Amenity).values()
     list_amenities = []
     for amenity in amenities:
@@ -20,7 +27,16 @@ def amenities():
 @app_views.route("/amenities/<amenity_id>", methods=['GET'],
                  strict_slashes=False)
 def amenities_id(amenity_id):
-    """Retrieves a Amenity object"""
+    """
+    Retrieves a specific Amenity object.
+
+    Args:
+        amenity_id (str): The ID of the Amenity object to retrieve.
+
+    Returns:
+        A JSON response with the specified Amenity object or a 404 error
+        if the Amenity object doesn't exist.
+    """
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
@@ -30,7 +46,16 @@ def amenities_id(amenity_id):
 @app_views.route("/amenities/<amenity_id>", methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(amenity_id):
-    """Deletes a Amenity object"""
+    """
+    Deletes a specific Amenity object.
+
+    Args:
+        amenity_id (str): The ID of the Amenity object to delete.
+
+    Returns:
+        An empty JSON response with a 200 status code if the deletion was
+        successful, or a 404 error if the Amenity object doesn't exist.
+    """
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
@@ -41,7 +66,15 @@ def delete_amenity(amenity_id):
 
 @app_views.route("/amenities", methods=['POST'], strict_slashes=False)
 def create_amenity():
-    """Creates a Amenity"""
+    """
+    Creates a new Amenity object.
+
+    Returns:
+        A JSON response with the newly created Amenity object and a 201
+        status code, or a JSON response with an error message and a 400
+        status code if the request body is invalid or missing a required
+        field.
+    """
     if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
     if "name" not in request.get_json():
@@ -56,7 +89,18 @@ def create_amenity():
 @app_views.route("/amenities/<amenity_id>", methods=['PUT'],
                  strict_slashes=False)
 def update_amenity(amenity_id):
-    """Updates a Amenity object"""
+    """
+    Updates a specific Amenity object.
+
+    Args:
+        amenity_id (str): The ID of the Amenity object to update.
+
+    Returns:
+        A JSON response with the updated Amenity object and a 200 status
+        code, or a 404 error if the Amenity object doesn't exist, or a
+        JSON response with an error message and a 400 status code if the
+        request body is invalid.
+    """
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
