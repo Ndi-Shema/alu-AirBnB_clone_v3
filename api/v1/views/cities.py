594ca4d390be91536cd6910a6193099cfc25999e
Ndi-Shema <f.shema@alustudent.com>
2023-04-27 07:27:14 +0200

    DBstorage and filestorage

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Module"""
+"""Flask module containing routes for CRUD operations on City objects."""
+
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response, abort, request
 from models import storage
-from flask import make_response, abort, request
 from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def cities_of_state(state_id):
-    """Retrieves the list of all City objects of a State"""
+    """
+    Retrieves a list of City objects that belong to a specific State object based on the provided state_id.
+
+    If the state does not exist, a 404 error will be returned.
+    """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
+
     list_cities = []
     for city in state.cities:
         list_cities.append(city.to_dict())
@@ -24,7 +28,11 @@ def cities_of_state(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def cities_id(city_id):
-    """Retrieves a City object."""
+    """
+    Retrieves a specific City object based on the provided city_id.
+
+    If the city does not exist, a 404 error will be returned.
+    """
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -34,19 +42,29 @@ def cities_id(city_id):
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
-    """Deletes a City object"""
+    """
+    Deletes a specific City object based on the provided city_id.
+
+    If the city does not exist, a 404 error will be returned.
+    """
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
+
     storage.delete(city)
     storage.save()
+
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def create_city(state_id):
-    """Creates a City"""
+    """
+    Creates a new City object and assigns it to a specific State object based on the provided state_id.
+
+    If the state does not exist, a 404 error will be returned.
+    If the request does not contain a JSON object or is missing required fields, a 400 error will be returned.
+    """
     if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
 
@@ -60,7 +78,8 @@ def create_city(state_id):
 
     req_body = request.get_json()
     req_body["state_id"] = state.id
-    # create city object
+
+    # Create a new City object
     city_obj = City(**req_body)
     storage.new(city_obj)
     storage.save()
@@ -70,7 +89,12 @@ def create_city(state_id):
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
-    """Updates a City object"""
+    """
+    Updates an existing City object based on the provided city_id.
+
+    If the city does not exist, a 404 error will be returned.
+    If the request does not contain a JSON object or is missing required fields, a 400 error will be returned.
+    """
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
